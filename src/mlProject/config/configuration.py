02a2dc966603d3978d48ae05de4ebc1a1974cb12
unknown <swpnil.sawant.me@gmail.com>
2024-01-11 14:03:42 +0530

    data validation & transformation

--- src/mlProject/config/configuration.py
+++ src/mlProject/config/configuration.py
@@ -1,7 +1,7 @@
 # copy this code and paste it into src/mlproject/config/configuration.py
 from mlProject.constants import *
 from mlProject.utils.common import read_yaml, create_directories
-from mlProject.entity.config_entity import DataIngestionConfig
+from mlProject.entity.config_entity import DataIngestionConfig, DataValidationConfig, DataTransformationConfig 
 
 
 
@@ -27,7 +27,7 @@ class ConfigurationManager:
 
         data_ingestion_config = DataIngestionConfig(
             root_dir=config.root_dir,
-            db_host=config.db_host,
+            db_host= config.db_host,
             db_user=config.db_user,
             db_password=config.db_password,
             db_name=config.db_name,
@@ -35,3 +35,32 @@ class ConfigurationManager:
         )
 
         return data_ingestion_config
+    
+    
+    def get_data_validation_config(self) -> DataValidationConfig:
+        config = self.config.data_validation
+        schema = self.schema.COLUMNS
+
+        create_directories([config.root_dir])
+
+        data_validation_config = DataValidationConfig(
+            root_dir=config.root_dir,
+            STATUS_FILE=config.STATUS_FILE,
+            unzip_data_dir = config.unzip_data_dir,
+            all_schema=schema,
+        )
+
+        return data_validation_config
+    
+    
+    def get_data_transformation_config(self) -> DataTransformationConfig:
+        config = self.config.data_transformation
+
+        create_directories([config.root_dir])
+
+        data_transformation_config = DataTransformationConfig(
+            root_dir=config.root_dir,
+            data_path=config.data_path,
+        )
+
+        return data_transformation_config
--- src/mlProject/entity/config_entity.py
+++ src/mlProject/entity/config_entity.py
@@ -9,3 +9,16 @@ class DataIngestionConfig:
     db_password: str
     db_name: str
     query: str
+
+
+@dataclass(frozen=True)
+class DataValidationConfig:
+    root_dir: Path
+    STATUS_FILE: str
+    unzip_data_dir: Path
+    all_schema: dict
+    
+@dataclass(frozen=True)
+class DataTransformationConfig:
+    root_dir: Path
+    data_path: Path
